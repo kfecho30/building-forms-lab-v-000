d895852a4e736a5c1ca394c016b7c6bcdc97b62e
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:45:52 +0000

    automatically backed up by learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class BandsContainer extends Component {
-  renderBands = () => this.props.bands.map((band, id) => `<li>${band.name}</li>`)
+  renderBands = () => this.props.store.getState().bands.map((band, id) => `<li>${band.name}</li>`)
 
   render() {
     return(
