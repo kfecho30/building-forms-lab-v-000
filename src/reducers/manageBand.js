c530f1fe7505e36d3729e96806f6907cac328668
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:57:56 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,7 +1,7 @@
 export default function manageBand(state = {bands: []}, action) {
   switch(action.type) {
     case "ADD_BAND":
-      return {bands: [...state.bands, action.band]}
+      return {...state, bands: [...state.bands, action.band]}
     default:
       return state
   }
