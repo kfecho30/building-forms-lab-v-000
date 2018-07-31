9aa65af2c51f30da2a72726de88a47561979559a
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:55:21 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,5 +3,11 @@ import React from 'react';
 const Bands = props => {
   const bands = props.bands.map((band, index) => {
     return <li key={index}>{band}</li>
-  })
+  });
+
+  return(
+    <div>
+      {bands}
+    </div>
+  )
 }
