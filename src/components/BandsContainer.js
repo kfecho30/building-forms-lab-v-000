f2c9bc4a6cd91c87bcde63d5e5b00fd030f86f75
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:48:01 +0000

    automatically backed up by learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
 export default class BandsContainer extends Component {
-  renderBands = () => this.props.store.getState().bands.map((band, id) => `<li key={id}>${band.name}</li>`)
-
   render() {
     return(
       <div>
         <ul>
-          {this.renderBands()}
+          <BandInput />
         </ul>
       </div>
     )
